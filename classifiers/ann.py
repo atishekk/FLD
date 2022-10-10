5ad1a200db3f5ce212cce62ef0637182bc1495f3
atishek <atishekkumar19@gmail.com>
2022-10-10 11:35:08 +0530

    training impl for ANN

--- classifiers/ann.py
+++ classifiers/ann.py
@@ -1,4 +1,5 @@
-from torch import nn
+import torch
+from torch import nn, optim
 
 class ANN(nn.Module):
     def __init__(self, in_features: int):
@@ -17,5 +18,25 @@ class ANN(nn.Module):
         X = self.flatten(X)
         return self.classifier(X)
 
-    def fit(self, X, y):
-        pass
+    def fit(self, X, y, epochs, batch_size=32, optimiser=optim.Adam, loss = nn.CrossEntropyLoss(), lr = 1e-6):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self = self.to(device, memory_format=torch.channels_last)
+        optim = optimiser(self.parameters(), lr=lr)
+        state = []
+
+        for epoch in range(epochs):
+            epoch_loss = 0
+            for idx in range(0, len(X), batch_size):
+                batch_x, batch_y = X[idx: idx+batch_size], y[idx: idx+batch_size]
+                batch_x, batch_y = batch_x.to(device, memory_format=torch.channels_last), batch_y.to(device, memory_format=torch.channels_last)
+                optim.zero_grad()
+                pred = self(batch_x)
+                ls = loss(pred, batch_y)
+                ls.backward()
+                optim.step()
+                with torch.no_grad():
+                    epoch_loss += ls.item()
+
+            state += [epoch_loss / len(X)]
+        return state
+
